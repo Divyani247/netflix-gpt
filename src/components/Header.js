1e6f1bd50da91c5a11bdc3ee0ec1ce9c894f10a5
Divyani247 <divyanihirulkar247@gmail.com>
2023-12-05 22:56:38 +0530

    updated browse page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,45 @@
 import { useState } from "react";
+import { onAuthStateChanged,} from "firebase/auth";
 import Dropdown from "./Dropdown";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { auth } from "../utils/firebase";
+import { addUser,removeUser } from "../utils/userSlice";
+import { useNavigate } from "react-router-dom";
+import { userAvatar } from "../utils/constant";
+
 const Header=()=>{
     const [isActive,setisActive]=useState(false);
-    const user=useSelector((store)=>store.user)
+    const dispatch=useDispatch();
+    const user=useSelector((store)=>store.user);
+    const navigate=useNavigate();
     const toggleDropdown = () => {
         setisActive(!isActive);
       };
+
+      useEffect(()=>{
+        const unsubscribe=onAuthStateChanged(auth, (user) => {
+            if (user) {
+              
+              const {uid,email,displayName} = user;
+              dispatch(addUser({uid:uid,email:email,displayName:displayName}));
+            
+              // ...
+            
+             navigate("/browse");
+            }
+            else {
+              // User is signed out
+              dispatch(removeUser());
+              navigate("/")
+            
+            }
+          });
+          return()=>unsubscribe();
+    },[]);
     return (
         <div className="absolute w-screen px-10 py-2 bg-gradient-to-b from-black flex justify-between">
-            <img className=" w-48 font-extrabold" src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" alt="logo"></img>
+            <img className=" w-48 font-extrabold" src={userAvatar} alt="logo"></img>
             {user && (
             <div className="flex flex-col p-2">
                <img className="w-8 h-8 ml-2 rounded-md" alt="usericon" onClick={toggleDropdown}  src="https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e "></img>
---
+++ src/utils/constant.js
@@ -0,0 +1,8 @@
+export const userAvatar="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+export const api_options= {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMjdkMDFjNWM3ZDFmOTcyYWZiMTZjY2FiYzE2NzczMyIsInN1YiI6IjY1NmVmMDJkODgwNTUxMDEzYTQ5ZDAwZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.r7bqDFO4olFXtNGNsYgjVRr-I836-CBK8incUpCKi-8'
+    }
+  };
